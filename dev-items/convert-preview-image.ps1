0ea162c8a6eaa3ff9ea922a34e6008db0e7da859
sk <content.type.text.net@gmail.com>
2026-04-08 19:43:29 +0900

    Image png (#13)
    * 原本的なものを追加
    
    * 画像変換用の窓口作成
    
    * 画像変換処理追加
    
    * preview.jpg は preview.png から生成するため git 管理外とした
    
    * preview.png と preview.jpg の扱いを記載

---
+++ dev-items/convert-preview-image.ps1
@@ -0,0 +1,23 @@
+﻿param(
+	[Parameter(Mandatory)][string] $Source,
+	[Parameter(Mandatory)][string] $Out,
+	[int] $Quality = 100
+)
+$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version Latest
+
+Add-Type -AssemblyName System.Drawing
+
+$image =[System.Drawing.Image]::FromFile($Source)
+try {
+	$encoder = [System.Drawing.Imaging.Encoder]::Quality
+	$encoderParams = New-Object System.Drawing.Imaging.EncoderParameters(1)
+	$encoderParams.Param[0] = New-Object System.Drawing.Imaging.EncoderParameter($encoder, $Quality)
+
+	$jpegCodec = [System.Drawing.Imaging.ImageCodecInfo]::GetImageEncoders() | Where-Object { $_.MimeType -eq 'image/jpeg' }
+
+	$image.Save($Out, $jpegCodec, $encoderParams)
+
+} finally {
+	$image.Dispose()
+}
