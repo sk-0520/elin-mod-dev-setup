537c4103c551a2eec33c3a26f771b1a266924956
sk <content.type.text.net@gmail.com>
2026-04-08 20:18:22 +0900

    ソリューションに対する削除プロジェクト追従

--- dev-items/initialize.ps1
+++ dev-items/initialize.ps1
@@ -20,6 +20,16 @@ foreach ($item in $removeItems) {
 
 # Mod 名をプロジェクトファイルに反映
 $projectPropsPath = Join-Path -Path $projectDirectoryPath -ChildPath "Directory.Build.props.user"
-[xml]$xml = Get-Content -LiteralPath $projectPropsPath -Raw -Encoding UTF8
-$xml.Project.PropertyGroup.AssemblyName = $ModName
-$xml.Save($projectPropsPath)
+[xml]$propsXml = Get-Content -LiteralPath $projectPropsPath -Raw -Encoding UTF8
+$propsXml.Project.PropertyGroup.AssemblyName = $ModName
+$propsXml.Save($projectPropsPath)
+
+# ソリューションから削除プロジェクトの破棄
+$solutionPath = Join-Path -Path $projectDirectoryPath -ChildPath "Elin.Plugin.slnx"
+[xml]$solutionXml = Get-Content -LiteralPath $solutionPath -Raw -Encoding UTF8
+$solutionXml.SelectNodes("//*/Project") | ForEach-Object {
+	if ($_.Path -eq "Elin.Plugin.Generator.Test/Elin.Plugin.Generator.Test.csproj") {
+		$_.ParentNode.RemoveChild($_) | Out-Null
+	}
+}
+$solutionXml.Save($solutionPath)
