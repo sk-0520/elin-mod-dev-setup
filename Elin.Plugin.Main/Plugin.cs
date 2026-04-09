95114ea688b09a1bc99c7a318df70dd7d2b6f771
sk <content.type.text.net@gmail.com>
2026-04-09 19:33:37 +0900

    コード修正 (#19)

--- Elin.Plugin.Main/Plugin.cs
+++ Elin.Plugin.Main/Plugin.cs
@@ -1,8 +1,7 @@
-#if DEBUG
 using Elin.Plugin.Main.Samples;
-#endif
 using HarmonyLib;
 using System;
+using System.Diagnostics;
 using System.Reflection;
 
 namespace Elin.Plugin.Main
@@ -17,10 +16,8 @@ namespace Elin.Plugin.Main
         /// <param name="harmony"></param>
         private void AwakePlugin(Harmony harmony)
         {
-#if DEBUG
             // サンプル用パッチ処理のため削除してください
             PatchSample(harmony);
-#endif
             //NOP
         }
 
@@ -34,8 +31,14 @@ namespace Elin.Plugin.Main
 
         #endregion
 
+        #region sample
 
-#if DEBUG
+        /// <summary>
+        /// サンプル用パッチ処理です。
+        /// </summary>
+        /// <remarks>不要なので削除してください。</remarks>
+        /// <param name="harmony"></param>
+        [Conditional("DEBUG")]
         void PatchSample(Harmony harmony)
         {
             // ネストクラスのフル名（例: Namespace.SourceElement+Row）
@@ -50,7 +53,7 @@ namespace Elin.Plugin.Main
 
             harmony.Patch(original, prefix: new HarmonyMethod(prefix));
         }
-#endif
 
+        #endregion
     }
 }
