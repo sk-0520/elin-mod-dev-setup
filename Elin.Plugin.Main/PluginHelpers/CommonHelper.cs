0e5d8fcc62983a8e57429559058af6aa3bda912a
sk <content.type.text.net@gmail.com>
2026-04-12 20:40:42 +0900

    共通処理だってよ！

---
+++ Elin.Plugin.Main/PluginHelpers/CommonHelper.cs
@@ -0,0 +1,10 @@
+namespace Elin.Plugin.Main.PluginHelpers
+{
+    /// <summary>
+    /// よく使う共通処理を集約しておく。
+    /// </summary>
+    /// <remarks>このプロジェクトでは定義するがこのプロジェクト内では使用しない。</remarks>
+    public class CommonHelper
+    {
+    }
+}
