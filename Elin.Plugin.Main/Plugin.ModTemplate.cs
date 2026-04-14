745f729f89b89afc0b8262620e89ca1cca5178e7
sk <content.type.text.net@gmail.com>
2026-04-14 16:29:52 +0900

    CallPatchAll

--- Elin.Plugin.Main/Plugin.ModTemplate.cs
+++ Elin.Plugin.Main/Plugin.ModTemplate.cs
@@ -9,6 +9,18 @@ namespace Elin.Plugin.Main
     [BepInPlugin(Package.Id, Mod.Name, Mod.Version)]
     public partial class Plugin : BaseUnityPlugin
     {
+        #region property
+
+        /// <summary>
+        /// <see cref="Harmony.PatchAll()"/>を呼び出すか。
+        /// </summary>
+        /// <remarks>
+        /// <para>デフォルトの<see langword="true"/>で問題ない。</para>
+        /// </remarks>
+        private bool CallPatchAll { get; set; } = true;
+
+        #endregion
+
         #region function
 
         /// <summary>
@@ -23,7 +35,10 @@ namespace Elin.Plugin.Main
 
             AwakePlugin(harmony);
 
-            harmony.PatchAll();
+            if (CallPatchAll)
+            {
+                harmony.PatchAll();
+            }
         }
 
         public void OnDestroy()
